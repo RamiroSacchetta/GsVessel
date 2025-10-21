8c7da210e174bc40f0ef58e0668653c69c8cc82e
valensacchetta <valen6sachhetta@gmail.com>
2025-10-21 09:44:55 -0300

    front

--- src/main/java/com/example/GSVessel/Security/WebConfig.java
+++ src/main/java/com/example/GSVessel/Security/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // aplica a todas las rutas
-                .allowedOrigins("http://localhost:3000") // tu frontend
+                .allowedOrigins("http://localhost:4200") // Tu frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
